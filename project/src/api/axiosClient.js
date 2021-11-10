88c05fc7ef44e55b1ce83725f9fe1686a6424313
Nguyen Hoan Bao <baohoannguyenblog01@gmail.com>
2021-11-11 01:29:04 +0700

    pagination done

--- project/src/api/axiosClient.js
+++ project/src/api/axiosClient.js
@@ -20,26 +20,28 @@ axiosClient.interceptors.request.use(
   }
 );
 
+// Add a request interceptor
+axios.interceptors.request.use(
+  function (config) {
+    // Do something before request is sent
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
 // Add a response interceptor
-axiosClient.interceptors.response.use(
+axios.interceptors.response.use(
   function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
-    return response.data;
+    return response;
   },
   function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
-    const { config, status, data } = error.response;
-    const URL = ["/auth/local/register", "/auth/local"];
-    if (URL.includes(config.url) && status === 400) {
-      const errorList = data.data || [];
-      const firstError = errorList.length > 0 ? errorList[0] : {};
-      const messageList = firstError.messages || [];
-      const firstMessages = messageList.length > 0 ? messageList[0] : {};
-      console.log(firstMessages);
-      throw new Error(firstMessages.message);
-    }
     return Promise.reject(error);
   }
 );
