292d4ef2190b7c4344735a9d28223f81adb15bc8
Nguyen Hoan Bao <baohoannguyenblog01@gmail.com>
2021-11-16 23:09:10 +0700

    footer changed

--- project/src/components/Footer/Footer.jsx
+++ project/src/components/Footer/Footer.jsx
@@ -15,8 +15,8 @@ const Footer = props => {
 
             <footer className="text-center text-lg-start text-muted page-footer">
 
-                <section
-                    className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
+                <div
+                    className="container d-flex justify-content-center justify-content-lg-between p-4"
                 >
 
                     <div className="me-5 d-none d-lg-block get-connected-title">
@@ -46,88 +46,86 @@ const Footer = props => {
                         </a>
                     </div>
 
-                </section>
+                </div>
+
+                <div className="border-bottom"></div>
+
+                <div className="container text-center text-md-start mt-5">
+
+                    <div className="row mt-3">
+
+                        <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4 col-item">
+
+                            <h6 className="text-uppercase fw-bold mb-4 col-item-title">
+                                <i className="fas fa-gem me-3"></i>Anrakutei
+                            </h6>
+                            <p>
+                                Here you can use rows and columns to organize your footer content. Lorem ipsum
+                                dolor sit amet, consectetur adipisicing elit.
+                            </p>
+                        </div>
 
 
 
-                <section className="">
-                    <div className="container text-center text-md-start mt-5">
+                        <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 col-item">
 
-                        <div className="row mt-3">
-
-                            <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4 col-item">
-
-                                <h6 className="text-uppercase fw-bold mb-4 col-item-title">
-                                    <i className="fas fa-gem me-3"></i>Anrakutei
-                                </h6>
-                                <p>
-                                    Here you can use rows and columns to organize your footer content. Lorem ipsum
-                                    dolor sit amet, consectetur adipisicing elit.
-                                </p>
-                            </div>
-
-
-
-                            <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 col-item">
-
-                                <h6 className="text-uppercase fw-bold mb-4 col-item-title">
-                                    Products
-                                </h6>
-                                <p>
-                                    <a href="#!" className="text-reset">Angular</a>
-                                </p>
-                                <p>
-                                    <a href="#!" className="text-reset">React</a>
-                                </p>
-                                <p>
-                                    <a href="#!" className="text-reset">Vue</a>
-                                </p>
-                                <p>
-                                    <a href="#!" className="text-reset">Laravel</a>
-                                </p>
-                            </div>
+                            <h6 className="text-uppercase fw-bold mb-4 col-item-title">
+                                Products
+                            </h6>
+                            <p>
+                                <a href="#!" className="text-reset">Angular</a>
+                            </p>
+                            <p>
+                                <a href="#!" className="text-reset">React</a>
+                            </p>
+                            <p>
+                                <a href="#!" className="text-reset">Vue</a>
+                            </p>
+                            <p>
+                                <a href="#!" className="text-reset">Laravel</a>
+                            </p>
+                        </div>
 
 
 
-                            <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4 col-item">
+                        <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4 col-item">
 
-                                <h6 className="text-uppercase fw-bold mb-4 col-item-title">
-                                    Useful links
-                                </h6>
-                                <p>
-                                    <a href="#!" className="text-reset">Pricing</a>
-                                </p>
-                                <p>
-                                    <a href="#!" className="text-reset">Settings</a>
-                                </p>
-                                <p>
-                                    <a href="#!" className="text-reset">Orders</a>
-                                </p>
-                                <p>
-                                    <a href="#!" className="text-reset">Help</a>
-                                </p>
-                            </div>
+                            <h6 className="text-uppercase fw-bold mb-4 col-item-title">
+                                Useful links
+                            </h6>
+                            <p>
+                                <a href="#!" className="text-reset">Pricing</a>
+                            </p>
+                            <p>
+                                <a href="#!" className="text-reset">Settings</a>
+                            </p>
+                            <p>
+                                <a href="#!" className="text-reset">Orders</a>
+                            </p>
+                            <p>
+                                <a href="#!" className="text-reset">Help</a>
+                            </p>
+                        </div>
 
 
 
-                            <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 col-item">
+                        <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 col-item">
 
-                                <h6 className="text-uppercase fw-bold mb-4 col-item-title">
-                                    Contact
-                                </h6>
-                                <p><i className="fas fa-home me-3"></i> New York, NY 10012, US</p>
-                                <p>
-                                    <i className="fas fa-envelope me-3"></i>
-                                    info@example.com
-                                </p>
-                                <p><i className="fas fa-phone me-3"></i> + 01 234 567 88</p>
-                                <p><i className="fas fa-print me-3"></i> + 01 234 567 89</p>
-                            </div>
-
+                            <h6 className="text-uppercase fw-bold mb-4 col-item-title">
+                                Contact
+                            </h6>
+                            <p><i className="fas fa-home me-3"></i> New York, NY 10012, US</p>
+                            <p>
+                                <i className="fas fa-envelope me-3"></i>
+                                info@example.com
+                            </p>
+                            <p><i className="fas fa-phone me-3"></i> + 01 234 567 88</p>
+                            <p><i className="fas fa-print me-3"></i> + 01 234 567 89</p>
                         </div>
 
                     </div>
-                </section>
+
+                </div>
 
 
 
