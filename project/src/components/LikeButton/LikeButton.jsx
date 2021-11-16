f4705e21ba24ed8acd39e1584d0804425f001a0c
Nguyen Hoan Bao <baohoannguyenblog01@gmail.com>
2021-11-16 22:57:13 +0700

    footer changed

--- project/src/components/LikeButton/LikeButton.jsx
+++ project/src/components/LikeButton/LikeButton.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart as fasHeart } from '@fortawesome/free-solid-svg-icons';
 import { faHeart as farHeart } from '@fortawesome/free-regular-svg-icons';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 const LikeButton = props => {
     const { value, onClick } = props;
